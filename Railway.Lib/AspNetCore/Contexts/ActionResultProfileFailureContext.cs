49944769cfc900cf661379dd850388a362028953
Сергей Сорокин <shenku@inbox.ru>
2024-08-13 21:03:08 +0300

    change class to record ActionResultProfileFailureContext

--- Railway.Lib/AspNetCore/Contexts/ActionResultProfileFailureContext.cs
+++ Railway.Lib/AspNetCore/Contexts/ActionResultProfileFailureContext.cs
@@ -2,12 +2,4 @@
 
 namespace Railway.Lib.AspNetCore.Contexts;
 
-public class ActionResultProfileFailureContext
-{
-    public ActionResultProfileFailureContext(Error error)
-    {
-        Error = error;
-    }
-
-    public Error Error { get; set; }
-}
+public record ActionResultProfileFailureContext(Error Error);
